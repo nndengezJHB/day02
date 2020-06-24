a7f09e49523430d347887a0875a56dfb12fc1aef
Nwabisa Ndengezi <nndengez@c4r3s3.wethinkcode.co.za>
2020-06-24 15:52:40 +0200

    ex04 day02

---
+++ ex04/ft_print_comb.c
@@ -0,0 +1,42 @@
+/*#include <stdio.h>
+#include <unistd.h>
+void	ft_putchar(char c)
+{
+		write(1, &c, 1);
+}	*/
+
+void	ft_print_comb(void)
+{
+	char	a;
+	char	b;
+	char	c;
+
+	a = '0' - 1;
+	while (++a <= '9')
+	{
+		b = a;
+		while (++b <= '9')
+		{
+			c = b + 1;
+			while (c <= '9')
+			{
+				ft_putchar(a);
+				ft_putchar(b);
+				ft_putchar(c);
+				if (a != '7' || b != '8' || c != '9')
+				{
+					ft_putchar(',');
+					ft_putchar(' ');
+				}
+				c++;
+			}
+		}
+	}
+}
+
+
+/*int     main()
+{
+    ft_print_comb();
+    return (0);
+}*/
